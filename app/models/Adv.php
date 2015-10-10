9f41a92a4b4aee9e7dc8c9ae15c2852bbb5e3ec9
jkzleond <jkzleond@163.com>
2015-10-10 14:34:38 +0800

    haha

--- app/models/Adv.php
+++ app/models/Adv.php
@@ -7,7 +7,45 @@ class Adv extends ModelEx
 	 */
 	public static function getIndexAdv()
 	{
-		$sql = 'select top 1 pic as pic_data from weladv where isState = 1 order by id desc';
+		$sql = <<<SQL
+		select
+            top 1    
+			id,pic,url,provinceId,
+			case clockType
+			when 1 then 1
+			when 4 then 2
+			when 3 then 3
+			when 2 then 4
+			else 99
+			end as priority
+		from WelAdv
+		where
+		  case clockType
+			when 4 then
+			  case
+				when datediff(mi,datename(yyyy, getdate()) + '-' + datename(mm, getdate()) + '-' + datename(dd, getdate()) + ' ' + repeatTime, getdate()) <= duration and datediff(mi,datename(yyyy, getdate()) + '-' + datename(mm, getdate()) + '-' + datename(dd, getdate()) + ' ' + repeatTime, getdate()) >= 0 then 1
+				else 0
+			  end
+			when 3 then
+			  case
+				when datediff(mi,datename(yyyy, getdate()) + '-' + datename(mm, getdate()) + '-' + repeatTime, getdate()) <= duration and datediff(mi,datename(yyyy, getdate()) + '-' + datename(mm, getdate()) + '-' + repeatTime, getdate()) >= 0 then 1
+				else 0
+			  end
+			when 2 then
+			  case
+				when datediff(mi,datename(yyyy, getdate()) + '-' + repeatTime, getdate()) <= duration and datediff(mi,datename(yyyy, getdate()) + '-' + repeatTime, getdate()) >= 0 then 1
+				else 0
+			  end
+			when 1 then
+			  case
+				when startTime <= getdate() and endTime >= getdate() then 1
+                when startTime <= getdate() and endTime is null then 1
+				else 0
+			  end
+			else 1
+		  end = 1 and isState = 1 and provinceId = 0
+		order by priority asc, createTime desc
+SQL;
 		return self::fetchOne($sql);
 	}
 }
