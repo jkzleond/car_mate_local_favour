5a921de57b2e561a9f2e8cb9ffc8676e5a1f1850
jkzleond <jkzleond@163.com>
2015-11-08 22:03:20 +0800

    haha

--- public/assets/js/app/main.js
+++ public/assets/js/app/main.js
@@ -24,7 +24,7 @@ require.config({
         views: 'app/views',
         templates: 'app/templates'
     },
-    urlArgs: 'bust=1.0433', //+ Date.now(),
+    urlArgs: 'bust=5.2', //+ Date.now(),
     waitSeconds: 0,
     shim:{
         backbone:{
