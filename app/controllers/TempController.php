9729697e0996aa842effc9ebed303758a1cb4cb7
jkzleond <jkzleond@163.com>
2015-11-25 02:23:27 +0800

    haha

--- app/controllers/TempController.php
+++ app/controllers/TempController.php
@@ -35,8 +35,8 @@ class TempController extends ControllerBase
 
 		$wx_state = $this->request->get('state', null, false);
 		$user_agent = $this->request->getUserAgent();
-		$is_wx = strpos($user_agent, 'MicroMessage') !== false;
-		echo $user_agent;exit;
+		$is_wx = preg_match('@MicroMessager@', $user_agent);
+		//echo $user_agent;exit;
 		//使用微信客户端访问,并且不是从授权页面跳转过来的(跳转过来都带state),重定向到授权页面
 		if($is_wx and !$wx_state)
 		{
