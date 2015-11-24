88e093882c53fb6d42eaf0aa46eb011984806d45
jkzleond <jkzleond@163.com>
2015-11-24 23:29:03 +0800

    haha

--- app/controllers/TempController.php
+++ app/controllers/TempController.php
@@ -57,8 +57,6 @@ class TempController extends ControllerBase
 				$wx_token_json = file_get_contents('https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$this->_app_id.'&secret='.$this->_app_secret.'&code='.$wx_code.'&grant_type=authorization_code');
 				$wx_token = json_decode($wx_token_json, true);
 				
-
-
 				$bind_user_list = User::getUserList(array(
 					'wx_opnenid' => $wx_token['openid']
 				));
--- app/models/User.php
+++ app/models/User.php
@@ -88,6 +88,9 @@ SQL;
         select id, userid as user_id, name, nickname, phone
         $condition_str
 SQL;
+        echo $sql.PHP_EOL;
+        print_r($bind);
+        exit;
         return self::nativeQuery($sql, $bind);
     }
 
