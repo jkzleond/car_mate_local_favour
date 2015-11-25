1f9754d0e7ebb1d9d4319c49b0e494d454fcdae9
jkzleond <jkzleond@163.com>
2015-11-25 16:29:37 +0800

    haha

--- app/controllers/TempController.php
+++ app/controllers/TempController.php
@@ -186,6 +186,7 @@ class TempController extends ControllerBase
 			$query_result->setFetchMode(Db::FETCH_ASSOC);
 			$involved_user = $query_result->fetch();
 			$is_already = !empty($involved_user);
+			return;
 			$this->view->setVar('is_already', $is_already);
 			if($is_already)
 			{
@@ -207,7 +208,6 @@ SQL;
 					$view_record_list = $view_result->fetchAll();
 					$this->view->setVar('view_record_list', $view_record_list);
 				}
-				return;
 				/*$this->flashSession->success('您已成功参加活动, 邀请码为[<span style="font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以分享给您的好友咯！<br/>(让TA为你做贡献O(∩_∩)O哈哈~)');
 				$this->view->setVar('p_user_phone', $user['phone']);
 				return;*/
