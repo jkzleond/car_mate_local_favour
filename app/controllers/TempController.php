77007621068fd0a44d463b7f89e04a2ed3b0c5ae
jkzleond <jkzleond@163.com>
2015-11-26 00:39:14 +0800

    haha

--- app/controllers/TempController.php
+++ app/controllers/TempController.php
@@ -9,7 +9,7 @@ class TempController extends ControllerBase
 
 	public function initialize()
 	{
-		ini_set('display_errors', 1);
+		ini_set('display_errors', 0);
 	}
 
 	public function insuranceShareDescribeAction()
@@ -22,6 +22,23 @@ class TempController extends ControllerBase
 	 */
 	public function insuranceShareAction()
 	{
+		$db = $this->db;
+		$get_view_sql = <<<SQL
+		select u.nickname, u.headimgurl from Hui_ActivityShareView v
+		left join WX_USER u on u.id = v.wx_user_id
+		where v.wx_user_id is not null and v.p_user_id = :p_user_id and v.aid = :aid
+SQL;
+		$get_view_bind = array(
+			'p_user_id' => 'jkzleond@163.com'
+			'aid' => 228
+		);
+		
+		$view_result = $db->query($get_view_sql, $get_view_bind);
+		$view_result->setFetchMode(Db::FETCH_ASSOC);
+		$view_record_list = $view_result->fetchAll();
+		print_r($view_record_list);exit;
+
+
 		$p_user_phone = $this->dispatcher->getParam('p_user_phone', null, '0');
 		$user_phone = $this->request->get('user_phone', null, null);
 		
@@ -203,17 +220,18 @@ class TempController extends ControllerBase
 				if($is_wx)
 				{
 					$get_view_sql = <<<SQL
-					select * from IAM_USER where userid = 'jkzleond@163.com'
-					
+					select u.nickname, u.headimgurl from Hui_ActivityShareView v
+					left join WX_USER u on u.id = v.wx_user_id
+					where v.wx_user_id is not null and v.p_user_id = :p_user_id and v.aid = :aid
 SQL;
 					$get_view_bind = array(
 						'p_user_id' => $p_user_id ? $p_user_id : '',
 						'aid' => 228
 					);
 					
-					$view_result = $db->query('select getdate() as [date]');
+					$view_result = $db->query($get_view_sql, $get_view_bind);
 					$view_result->setFetchMode(Db::FETCH_ASSOC);
-					$view_record_list = $view_result->fetch();
+					$view_record_list = $view_result->fetchAll();
 					$this->view->setVar('view_record_list', $view_record_list);
 				}
 
