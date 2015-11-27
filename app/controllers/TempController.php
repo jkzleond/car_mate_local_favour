b3c28f0068ed81a5773c7552b66914ce15418317
jkzleond <jkzleond@163.com>
2015-11-27 15:26:04 +0800

    haha

--- app/controllers/TempController.php
+++ app/controllers/TempController.php
@@ -261,11 +261,11 @@ SQL;
 
 				if($p_user_id)
 				{
-					$this->flashSession->success('您也获得了邀请码哦！<br/> 邀请码为[<span style="color:orange;font-size:1.5em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！');
+					$this->flashSession->success('您也获得了邀请码哦！<br/> 邀请码为[<span style="color:orange;font-size:1.2em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！');
 				}
 				else
 				{
-					$this->flashSession->success('您已成功参加活动<br/>邀请码为[<span style="color:orange;font-size:1.5em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！<br/>成功邀请<b style="color:yellow">20</b>个好友购买保险，您的车险就可以免单啦');
+					$this->flashSession->success('您已成功参加活动<br/>邀请码为[<span style="color:orange;font-size:1.2em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！<br/>成功邀请<b style="color:yellow">20</b>个好友购买保险，您的车险就可以免单啦');
 				}
 				$this->view->setVar('p_user_phone', $user['phone']);
 				return;
@@ -285,11 +285,11 @@ SQL;
 
 			if($p_user_id)
 			{
-				$this->flashSession->success('您也获得了邀请码哦！<br/> 邀请码为[<span style="color:orange;font-size:1.5em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！');
+				$this->flashSession->success('您也获得了邀请码哦！<br/> 邀请码为[<span style="color:orange;font-size:1.2em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！');
 			}
 			else
 			{
-				$this->flashSession->success('您已成功参加活动<br/>邀请码为[<span style="color:orange;font-size:1.5em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！<br/>成功邀请<b style="color:yellow">20</b>个好友购买保险，您的车险就可以免单啦');
+				$this->flashSession->success('您已成功参加活动<br/>邀请码为[<span style="color:orange;font-size:1.2em;font-weight:bold">'.$involved_user['invitation_code'].'</span>], 可以点击右上角分享给您的好友，也可以将邀请码告知您的好友，在车友惠精算时填写邀请码！<br/>成功邀请<b style="color:yellow">20</b>个好友购买保险，您的车险就可以免单啦');
 			}
 			$this->view->setVar('p_user_phone', $user['phone']);
 			$this->view->setVar('is_success', true);
